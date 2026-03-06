72b18fc06df8cbf9fed8548da0f3f00c25efb528
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2026-03-06 22:01:57 +0300

    add custom font

---
+++ Thmanyah/Core/Helper/Font+Extension.swift
@@ -0,0 +1,46 @@
+//
+//  Font+Extension.swift
+//  Thmanyah
+//
+//  Created by Ahmed.Abdraboh on 06/03/2026.
+//
+
+import SwiftUI
+
+enum FontFamily {
+    case bold
+    case regular
+    case medium
+    case thin
+}
+
+extension Font {
+    static func ibmArabicBold(_ size: CGFloat) -> Font {
+        .custom("IBMPlexSansArabic-Bold", size: size)
+    }
+
+    static func ibmArabicRegular(_ size: CGFloat) -> Font {
+        .custom("IBMPlexSansArabic-Regular", size: size)
+    }
+
+    static func ibmArabicMedium(_ size: CGFloat) -> Font {
+        .custom("IBMPlexSansArabic-Medium", size: size)
+    }
+
+    static func ibmArabicThin(_ size: CGFloat) -> Font {
+        .custom("IBMPlexSansArabic-Thin", size: size)
+    }
+
+    static func appFont(size: CGFloat, _ family: FontFamily = .regular) -> Font {
+        switch family {
+        case .bold:
+            return .ibmArabicBold(size)
+        case .regular:
+            return .ibmArabicRegular(size)
+        case .medium:
+            return .ibmArabicMedium(size)
+        case .thin:
+            return .ibmArabicThin(size)
+        }
+    }
+}

c47f084ad38f9273a83388df1911381ab6839da5
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2026-03-06 22:02:15 +0300

    edit ui

---
+++ Thmanyah/ThmanyahApp.swift
@@ -0,0 +1,19 @@
+//
+//  ThmanyahApp.swift
+//  Thmanyah
+//
+//  Created by Ahmed.Abdraboh on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ThmanyahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            let homeUseCase = DefaultHomeUseCase(homeRepository: HomeRepository(homeService: HomeService()))
+            let viewModel = HomeViewModel(homeUseCase: homeUseCase)
+            HomeView(viewModel: viewModel)
+        }
+    }
+}
